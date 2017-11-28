b0142b17221342a033f7d8795fd7660fc7068625
Liam O'Gorman <liam@liamogorman.com>
2017-11-28 00:50:37 -0800

    Configuration tweaks

--- config.py
+++ config.py
@@ -7,19 +7,25 @@ class Config():
     # Max number of frames to combine when searching
     # for lanes.
     SMOOTH_OVER_N_FRAMES = 2
-    
+
     # Number of windows for the sliding window search
     SEARCH_WINDOWS = 9
     
     # Search window margin for both the sliding window and
     # fast fit search.
-    MARGIN = 120
+    MARGIN = 75
+
+    #Minimum number of pixels in a window to recenter and fit to.
+    MIN_PIXEL_PER_WINDOW = 75
+
     # If 5 frames in a row don't have a good lane match
     # reset to sliding window search
     MAX_REJECTED = 5
 
-    # meters per pixel in y dimension
-    YM_PER_PIX = 35 / 720
+    # meters per pixel in y dimension 
+    # Measured with output images from straight_line1.jpg warped with same
+    # perspective. 3m Lane line is 60px long.
+    YM_PER_PIX = 3 / 60
     
     # meters per pixel in x dimension
     # Calculated from the warped perspective transform used.
@@ -39,9 +45,10 @@ class Config():
     MAX_SMOOTH_SAMPLES = 5
 
     # Maximum standard deviation (in pixels) below which we
-    # consider as parallel therefor a good lane detection. 
+    # consider as parallel therefore a good lane detection. 
     PROBABLE_LANE_WIDTH_STDDEV = 40
     
     # Range of average lane widths to accept as a probable lane
+    # Larger lanes more probable than narrower lanes.
     PROBABLE_LANE_AVERAGE_WIDTH_RANGE = (
-        LANE_WIDTH_IN_PIXELS - 50, LANE_WIDTH_IN_PIXELS + 50)
+        LANE_WIDTH_IN_PIXELS - 75, LANE_WIDTH_IN_PIXELS + 150)
